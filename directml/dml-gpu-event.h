bc958d199ad290e3e0a0e7fa29e800f70faa3705
Patrice Vignola <vignola.patrice@gmail.com>
2024-06-10 12:53:13 -0700

    Fix formatting

--- directml/dml-gpu-event.h
+++ directml/dml-gpu-event.h
@@ -19,13 +19,13 @@ struct GpuEvent
     // Blocks until IsSignaled returns true.
     void WaitForSignal() const {
         if (IsSignaled()) {
-        return;  // early-out
+            return;  // early-out
         }
 
         while (!IsSignaled()) {
-    #if defined(_M_AMD64) || defined(__x86_64__)
-        _mm_pause();
-    #endif
+#if defined(_M_AMD64) || defined(__x86_64__)
+            _mm_pause();
+#endif
         }
     }
 };
